3b954eb2de12cdc1bbfe4905fa174320310a4229
Alex the mega one <alexandre.bernat@ga.ita.br>
2021-06-08 23:54:11 -0300

    Projeto Da Atividade Avaliativa, updates

--- SistemaBanco.py
+++ SistemaBanco.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from random import randint
 
 
 class Order(ABC): # Abstract Command
@@ -33,17 +34,47 @@ class Transfere(Order): # Concrete Command
 
 
 class Sistema: # Receiver
+    def __init__(self):
+        self.saldo = self.getSaldo()
+        self.gastos = self.getGastos()
+
     def mostrarExtrato(self):
-        print("Você está vendo seu extrato. Trate de economizar.")
+        print("Você está vendo seu extrato. Gastando muito com Ifood!.")
+        print("Dia | Gasto")
+        for i in range(0, len(self.gastos)):
+            print(str(i) + " " + str(self.gastos[i]))
 
     def mostrarSaldo(self):
         print("Você está completamente milionário e seu saldo segue abaixo.")
+        print(self.saldo)
 
     def envia(self, valor):
+        if (self.saldo < valor):
+            print("Consiga mais dinheiro. Saldo insuficiente")
+            return
         print(str(valor) + " " + "reais enviados para o destino solicitado. Aproveite as novidades com PIX!")
+        self.updateSaldo(valor, '-')
+        self.mostrarSaldo()
+
+    def getSaldo(self):
+        # Em um sistema de verdade, aqui puxaria do DB
+        valor = 929478/25
+        return valor
+
+    def getGastos(self):
+        gastos = []
+        for i in range(0, 30):
+            gastos.append(randint(100, 150))
+        return gastos
+
+    def updateSaldo(self, valor, op):
+        if op == '+':
+            self.saldo = self.saldo+valor
+        if op == '-':
+            self.saldo = self.saldo-valor
 
 
-class Agent: #Invoker
+class Agent: # Invoker
     def __init__(self):
         self.__order_queue = []
 
@@ -56,7 +87,7 @@ if __name__ == "__main__":
     #Cliente
     sistema = Sistema()
     ver_saldo = VerificaSaldo(sistema)
-    ver_extrato = VerificaExtrato(sistema)
+    ver_extrato = VerificaExtrato(sistema, )
     transfere_valor = Transfere(sistema, 1000)
 
     #Agente
